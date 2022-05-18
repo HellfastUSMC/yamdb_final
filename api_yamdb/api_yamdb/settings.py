c277859e15ea885f1fb5ba368146a3893227d334
HellfastUSMC <4epenki@gmail.com>
2022-05-18 10:43:32 +0300

    fix bugs and testing

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -12,7 +12,7 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY', default='12345')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 FROM_EMAIL = 'some@mail.ru'
 
